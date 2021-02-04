45b21c9eb0a4966d62a93eb4fa94fd636a34c927
Łukasz Kidoń <lkidon4@gmail.com>
2021-02-04 22:30:08 +0100

    Add final version

--- js/script.js
+++ js/script.js
@@ -33,13 +33,7 @@ if(playerInput == '1'){
 printMessage('Twój ruch to: ' + playerMove);
 
 
-if( computerMove == 'kamień' && playerMove == 'kamień'){
-    printMessage('Remis!');
-} else if(computerMove == 'papier' && playerMove == 'papier'){
-    printMessage('Remis!');
-} else if(computerMove == 'nożyce' && playerMove == 'nożyce'){
-    printMessage('Remis!');
-} else if(computerMove == 'kamień' && playerMove == 'papier'){
+if(computerMove == 'kamień' && playerMove == 'papier'){
     printMessage('Ty wygrywasz!');
 } else if(computerMove == 'papier' && playerMove == 'nożyce'){
     printMessage('Ty wygrywasz!');
